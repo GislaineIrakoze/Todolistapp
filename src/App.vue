d9c1a63154c12896312bbde84fc31a7efa098389
Irakoze Mahoro Gislaine <gislaineirakoze90@gmail.com>
2026-01-17 21:32:49 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
-  <HelloWorld />
+  <div class="app">
+    <h1>My Awesome App</h1>
+  </div>
 </template>
 
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
 </script>
+
+<style scoped>
+.app {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+h1 {
+  font-size: 3rem;
+}
+</style>
